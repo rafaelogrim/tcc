450c5244d43485d2509e19ccfe4278efe5a7128b
Rafael Ogrim <rafael.ogrim@gmail.com>
2020-05-31 23:41:37 -0300

    - commit

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -13,7 +13,7 @@ module.exports = {
             MONGO_URI: 'mongodb://localhost:27017/projeto-inst',
         },
         env_production: {
-            PORT: 80,
+            PORT: 8080,
             NODE_ENV: 'production',
             MONGO_URI: 'mongodb://projetoinst:9oZwKeF9QSYu2hDiDjuuxh8pYft49gclWEgsKdUpA1d7rrYCAxD3iPdbWWuyurcYhpBqndJ0LGo0nvq1t39ldg==@projetoinst.documents.azure.com:10255/?ssl=true',
         }
